5be6f5963811907e2b298fea48966cea4710925d
Krystian <krystian1wojtowicz@gmail.com>
2023-04-10 19:50:02 +0200

    test should render with items in the cart

--- src/components/Basket/Basket.test.tsx
+++ src/components/Basket/Basket.test.tsx
@@ -4,9 +4,10 @@ import {
   waitFor,
   act,
   screen,
+  renderHook,
 } from "@testing-library/react";
 import { Basket } from "./Basket";
-import { OrderType } from "../../Interface";
+// import { OrderType } from "../../Interface";
 import { useState, useEffect } from "react";
 import { OrderContext } from "../OrderContext";
 
@@ -14,7 +15,36 @@ describe("Basket", () => {
   const addPizza = jest.fn();
   const removePizza = jest.fn();
   const onSubmit = jest.fn();
-  
+
+  it("should render with items in the cart", () => {
+    const cartItems = [
+      { id: "1", name: "Pizza Americana", price: 10, qty: 2 },
+      { id: "2", name: "Pizza Margherita", price: 8, qty: 1 },
+    ];
+    const setCartItems = jest.fn();
+    const order = {
+      customerName: "",
+      city: "",
+      street: "",
+      numberOfStreet: "",
+      numberOfFlat: "",
+      date: "",
+      email: "",
+      phone: "",
+    };
+    const setOrder = jest.fn();
+
+    const { getByText } = render(
+      <OrderContext.Provider
+        value={{ cartItems, setCartItems, order, setOrder }}
+      >
+        <Basket addPizza={addPizza} removePizza={removePizza} />
+      </OrderContext.Provider>
+    );
+
+    expect(getByText("Pizza Americana")).toBeInTheDocument();
+    expect(getByText("Pizza Margherita")).toBeInTheDocument();
+  });
   it("calls the onSubmit function", async () => {
     const { getByLabelText } = render(
       <Basket
@@ -55,12 +85,9 @@ describe("Basket", () => {
       expect(onSubmit).toHaveBeenCalledWith({ lazy: true });
     });
   });
-  test("renders component corectly", () => {
+  it("renders component corectly", () => {
     const { getByRole } = render(
-      <Basket
-        addPizza={addPizza}
-        removePizza={removePizza}
-      />
+      <Basket addPizza={addPizza} removePizza={removePizza} />
     );
     const button = screen.getByText(/submit/i);
     expect(button).toBeInTheDocument();
